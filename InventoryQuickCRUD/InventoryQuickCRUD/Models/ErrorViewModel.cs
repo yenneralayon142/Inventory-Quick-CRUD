8c891eef80e26be1e5e0b7df059bcbb1e60cf378
yenneralayon142 <yenner.alayon1@misena.edu.co>
2022-06-08 22:35:22 -0500

    Agrego paquetes nuggets

---
+++ InventoryQuickCRUD/InventoryQuickCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventoryQuickCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
